100423ea0cdd63de773a9496c78365be8fc61b7c
Suyash2175 <127495941+Suyash2175@users.noreply.github.com>
2024-06-17 19:23:17 +0530

    Create ifelse.js
    ifelse,else if ,switch,

---
+++ ifelse.js
@@ -0,0 +1,101 @@
+///******* CONDITIONALS IN JS
+//IF ELSE
+
+// let age=19;
+// if (age <18){
+//     console.log('you cannot drive');
+// }else{
+//     console.log('u can get license')
+// }
+
+
+// ///
+// let number=10;
+// if(number == 10){
+//     console.log('number is 10')
+// }else {
+//     console.log('number is not equal')
+// }
+/// comparison operators are used to compare values
+
+//1.> greater than 
+//2.< less than
+//3.>= grater than or equal to
+//4.<= less than or equal to
+//5. == equal to
+//6.!= not equal to
+//7.=== equal value or not equal type
+//8.!== not equal value or not equal types
+
+//1 eg
+
+// let age =18;
+// if(age != 18){
+//     console.log('age is  equal to 18')
+// }else{
+//     console.log('age is not equal to 18')
+// }
+
+///ELSE IF CONDITIONAL
+//were we have to check two or more conditions use else if
+// let age=15;
+// if(age ==18){
+//     console.log('age is 18');
+// }else if(age > 18){
+//     console.log('ahe is grater than 18');
+
+// }else{
+//     console.log('age is less than 18');
+
+// }
+
+
+/// LOGICAL OPERATORS
+//&& -AND OPERATOR
+// || - OR OPERATOR
+// we have check 2 or more conditions together
+// // && in and if both conditions are true then only code will execuite
+// let age=70;
+// if(age > 18 || age < 65){
+//     console.log('your safe to drive')
+
+// } else {
+//     console.log('your not safe to drive')
+// }
+
+// //SWITCH ///
+
+// let fruit='apple';
+// switch(fruit){
+//     case 'apple':
+//         console.log('your fav fruit is apple')
+//         case 'mango':
+//             console.log('your fav fruit is mango')
+//    case 'banana':
+//     console.log('your fav fruit is banana')
+
+// }
+
+//### for loop####
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
