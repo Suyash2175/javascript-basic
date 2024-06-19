d0d2ac3e93c595292a68cbdaa8431c814502f342
Suyash2175 <127495941+Suyash2175@users.noreply.github.com>
2024-06-19 15:56:00 +0530

    Create for loop.js

---
+++ for loop.js
@@ -0,0 +1,142 @@
+///******* CONDITIONALS IN JS
+//IF ELSE
+
+// let age=19;
+// if (age <18){
+//     console.log('you cannot drive');
+// }else{
+//     console.log('u can get license')
+// }
+
+
+// ///
+// let number=10;
+// if(number == 10){
+//     console.log('number is 10')
+// }else {
+//     console.log('number is not equal')
+// }
+/// comparison operators are used to compare values
+
+//1.> greater than 
+//2.< less than
+//3.>= grater than or equal to
+//4.<= less than or equal to
+//5. == equal to
+//6.!= not equal to
+//7.=== equal value or not equal type
+//8.!== not equal value or not equal types
+
+//1 eg
+
+// let age =18;
+// if(age != 18){
+//     console.log('age is  equal to 18')
+// }else{
+//     console.log('age is not equal to 18')
+// }
+
+///ELSE IF CONDITIONAL
+//were we have to check two or more conditions use else if
+// let age=15;
+// if(age ==18){
+//     console.log('age is 18');
+// }else if(age > 18){
+//     console.log('ahe is grater than 18');
+
+// }else{
+//     console.log('age is less than 18');
+
+// }
+
+
+/// LOGICAL OPERATORS
+//&& -AND OPERATOR
+// || - OR OPERATOR
+// we have check 2 or more conditions together
+// // && in and if both conditions are true then only code will execuite
+// let age=70;
+// if(age > 18 || age < 65){
+//     console.log('your safe to drive')
+
+// } else {
+//     console.log('your not safe to drive')
+// }
+
+// //SWITCH ///
+
+// let fruit='apple';
+// switch(fruit){
+//     case 'apple':
+//         console.log('your fav fruit is apple')
+//         case 'mango':
+//             console.log('your fav fruit is mango')
+//    case 'banana':
+//     console.log('your fav fruit is banana')
+
+// }
+
+//### for loop####
+// for (let i=0; i <5;i++){
+//     console.log(i ,'hello suyash');
+// }
+// for(let i=3;i>0;i--){
+//     console.log(i,'hello');
+// }
+
+// let fruits=['apple','banana','mango']
+// for (let i=0;i<fruits.length;i++){
+//     console.log(fruits[i])
+// }
+// ///while loop#####
+// let x=1;
+// while(x<3){
+//     console.log('suyash bro',x);
+//     x++;
+// }
+
+// ///for of loop//
+ let fruits=['apple','banana','mango']
+
+for (fruit of fruits){
+    console.log(fruit);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
