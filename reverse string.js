b74994006edc313a1ab029499b586ad70e552a80
Suyash2175 <127495941+Suyash2175@users.noreply.github.com>
2024-07-08 18:25:48 +0530

    Create reverse string.js

---
+++ reverse string.js
@@ -0,0 +1,11 @@
+// Function to reverse a string
+function reverseString(str) {
+    return str.split('').reverse().join('');
+}
+
+// Test the function
+let originalString = "Hello, World!";
+let reversedString = reverseString(originalString);
+
+console.log(`Original String: ${originalString}`); // Output: "Hello, World!"
+console.log(`Reversed String: ${reversedString}`); // Output: "!dlroW ,olleH"
