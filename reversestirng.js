16834e7dc85b7e40922d2acd47960eaad9e5b5a1
Suyash2175 <127495941+Suyash2175@users.noreply.github.com>
2024-07-29 21:49:19 +0530

    Create reversestirng.js

---
+++ reversestirng.js
@@ -0,0 +1,7 @@
+function reverseString(s) {
+  return s.split('').reverse().join('');
+}
+
+// Test cases
+console.log(reverseString("hello")); // Output: "olleh"
+console.log(reverseString("JavaScript")); // Output: "tpircSavaJ"
