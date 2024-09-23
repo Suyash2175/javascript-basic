18c1f4c0f89aa66033130f945f366e17fdcc3ade
Suyash2175 <127495941+Suyash2175@users.noreply.github.com>
2024-09-23 15:22:26 +0530

    Create firstUniqueChar.js

---
+++ firstUniqueChar.js
@@ -0,0 +1,10 @@
+function firstNonRepeatingChar(str) {
+    for (let i = 0; i < str.length; i++) {
+        if (str.indexOf(str[i]) === str.lastIndexOf(str[i])) {
+            return str[i];
+        }
+    }
+    return null;
+}
+
+console.log(firstNonRepeatingChar("aabbcdd")); // Output: "c"
