2d7e96fe7900d8cbffb5183d15432562450fd3fc
Suyash2175 <127495941+Suyash2175@users.noreply.github.com>
2024-09-29 22:48:34 +0530

    Create . Read and Write Files Using fs.js

---
+++ . Read and Write Files Using fs.js
@@ -0,0 +1,20 @@
+const fs = require('fs');
+
+function readAndWriteFiles(inputPath, outputPath) {
+    fs.readFile(inputPath, 'utf8', (err, data) => {
+        if (err) {
+            console.error(err);
+            return;
+        }
+        fs.writeFile(outputPath, data, (err) => {
+            if (err) {
+                console.error(err);
+                return;
+            }
+            console.log('File written successfully');
+        });
+    });
+}
+
+// Usage
+readAndWriteFiles('input.txt', 'output.txt');
