14f57b9f33a48fd5289efa9e14d7ace4bd10b946
Suyash2175 <127495941+Suyash2175@users.noreply.github.com>
2024-10-04 22:21:33 +0530

    Create reversestring.js

---
+++ reversestring.js
@@ -0,0 +1,7 @@
+function reverseString(str) {
+    return str.split('').reverse().join('');
+}
+
+// Example usage
+const inputString = "Hello";
+console.log("Reversed string:", reverseString(inputString));
