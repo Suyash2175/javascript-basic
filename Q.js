cd4b954c6cc80a5abbc033107f8df34a4455d3b0
Suyash2175 <127495941+Suyash2175@users.noreply.github.com>
2024-12-01 14:03:24 +0530

    Create Q.js

---
+++ Q.js
@@ -0,0 +1,45 @@
+file-naming convention specified below.
+
+1. Perform EDA and visualization on the data Desktop/01 cav. Nite leve the file name prefix Q1-for your submission
+
+(A Market
+
+2. Perform multiple linear regression on the data in Desktop/02 cay with Y as the reponse variatile and the rest as predictors. Use the file name prefix 02- for your submission
+
+(a) Obtain the fit.
+
+(1 emarke)
+
+(b) Interpret the summary of the fit
+
+(2 marks)
+
+(e) Make a plot of fitted values vs, observed responses.
+
+(3 mark)
+
+(d) Present regression diagnostics. Record relevant observations with respect to outliers, influential points, nonconstant variance, nonlinearity, nonnormality, etc.
+
+(3 Mark)
+
+3. Perform multiple logistic regression on the data in Desktop/Q3.cay with Y as the response variable and the rest as predictors. Use the file name prefix 03- for your submission.
+
+(a) Split the data as 80% (train) and 20% (test).
+
+(1 mark)
+
+(b) Obtain the fit using the training data
+
+(1 mark)
+
+(c) Interpret the summary of the fit.
+
+(2 marka)
+
+(d) Make predictions on the test data.
+
+(1 marks)
+
+(e) Present confusion matrix, accuracy, TPR. FPR, TNR, FNR. Interpret the results.
+
+(4 marlos)
